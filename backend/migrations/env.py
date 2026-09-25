595fad21a6befa18f6d3f3287ad7ee5eebda8a5b
martinjoo <martin@martinjoo.dev>
2026-09-25 09:54:50 +0200

    Add video models

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -3,9 +3,14 @@ from logging.config import fileConfig
 
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
-from sqlalchemy.ext.asyncio import async_engine_from_config
+from sqlalchemy.ext.asyncio import async_engine_from_config, create_async_engine
 
 from alembic import context
+
+from ytkb.core.config import settings
+from ytkb.core.models import Base
+
+import ytkb.apps.videos.models
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -20,7 +25,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -40,9 +45,8 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
     context.configure(
-        url=url,
+        url=settings.database_url,
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
@@ -65,11 +69,7 @@ async def run_async_migrations() -> None:
 
     """
 
-    connectable = async_engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
+    connectable = create_async_engine(settings.database_url, poolclass=pool.NullPool)
 
     async with connectable.connect() as connection:
         await connection.run_sync(do_run_migrations)
---
+++ backend/src/ytkb/apps/videos/models.py
@@ -0,0 +1,53 @@
+from typing import List, Optional
+
+from sqlalchemy.orm import Mapped, relationship, mapped_column
+from sqlalchemy import ForeignKey, String, Integer
+
+from ytkb.core.models import Base
+
+class Channel(Base):
+    __tablename__ = "videos__channels"
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100))
+    url: Mapped[str] = mapped_column(String(150))
+    
+    videos: Mapped[List["Video"]] = relationship(
+        back_populates="channel", 
+        cascade="all, delete-orphan",
+    )
+    
+    def __repr__(self):
+        return f"Channel(id={self.id}, name={self.name}, url={self.url})"
+    
+class Video(Base):
+    __tablename__ = "videos__videos"
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(200))
+    url: Mapped[str] = mapped_column(String(250))
+    content: Mapped[Optional[str]]
+    channel_id: Mapped[int] = mapped_column(ForeignKey("videos__channels.id"))
+
+    channel: Mapped["Channel"] = relationship(back_populates="videos")
+    chunks: Mapped[List["VideoChunk"]] = relationship(
+        back_populates="video", 
+        cascade="all, delete-orphan",
+    )
+    
+    def __repr__(self):
+        return f"Video(id={self.id}, title={self.title}, url={self.url})"
+    
+class VideoChunk(Base):
+    __tablename__ = "videos__video_chunks"
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    position: Mapped[int] = mapped_column(Integer())
+    content: Mapped[str]
+    video_id: Mapped[int] = mapped_column(ForeignKey("videos__videos.id"))
+
+    video: Mapped["Video"] = relationship(back_populates="chunks")
+    
+    def __repr__(self):
+        return f"VideoChunk(id={self.id}, video_id={self.video_id}, position={self.position}, content={self.content[:100]}...)"
+    
